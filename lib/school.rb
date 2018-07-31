904b928c8da74168c216eb314fbae922e3726c6a
Koy Saeteurn <kosatun@aol.com>
2018-07-31 05:01:45 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,14 @@
 # code here!
 class School
-  attr_accessor
+  attr_accessor :add_student
+  attr_reader :name, :roster 
+  
+  ROSTER = {}
   
   def initialize(name)
     @name = name 
   end  
   
-  def roster
-    {}
-  end   
   
   def add_student(name, grade)
     
